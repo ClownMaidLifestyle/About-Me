4ae7a8d8cdd7e385e3c483d332852ef5de05687d
Clownmaidlifestyle <e.edson@protonmail.ch>
2023-06-13 15:40:25 +0100

    question 2

--- script.js
+++ script.js
@@ -13,14 +13,27 @@ function quiz(){
     while (question == 1){
         answerCache = prompt("question 1 \n The newest country on my music map is Denmark");
         if (answerCache.toUpperCase == ("YES" || "Y")){
-            alert("False! I'm a long time Situationsfornærmelse fan" + answerCache);
+            alert("False! I'm a long time Situationsfornærmelse fan");
             question++;
-            answers.push(answerCache);
         }
         else if (answerCache.toUpperCase() == ("NO" || "N")){
             alert("So True Bestie! Austria is the newest as their Eurovision entry popped off");
             question++;
-            answers.push(answerCache);
+        }
+        else{
+            alert("Something went wrong!! Please try again");
+        }
+    }
+
+    while (question == 2){
+        answerCache = prompt("question 2 \n I can make a banging pizza");
+        if (answerCache.toUpperCase == ("YES" || "Y")){
+            alert("So True Bestie! I can make ten pizza bases at once");
+            question++;
+        }
+        else if (answerCache.toUpperCase() == ("NO" || "N")){
+            alert("False! I was a pizza chef for a year");
+            question++;
         }
         else{
             alert("Something went wrong!! Please try again");
