814bdf42c82aeeb46ede00a776351c8247a6f030
Clownmaidlifestyle <e.edson@protonmail.ch>
2023-06-13 15:34:06 +0100

    question 1 quiz

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ function load(){
     username = prompt("Hello bestie, what shall I call you?");
     alert("Welcome, Bestie " + username);
 }
+
+function quiz(){
+    let answers = [];
+    let answerCache = "";
+    let question = 1;
+    answerCache = prompt("question 1 \n The newest country on my music map is Denmark")
+    while (question == 1){
+        if (answerCache.toUpperCase == ("YES" || "Y")){
+            alert("False! I'm a long time Situationsfornærmelse fan" + answerCache);
+            question++;
+            answers.push(answerCache);
+        }
+        else if (answerCache.toUpperCase() == ("NO" || "N")){
+            alert("So True Bestie! Austria is the newest as their Eurovision entry popped off");
+            question++;
+            answers.push(answerCache);
+        }
+        else{
+            alert("Something went wrong!! Please try again");
+        }
+    }
+}
+        
